5bbaab17f9eaeff6366b404b0021f440214f7f15
Sandi Miftah <sandiseeyou@gmail.com>
2026-05-12 23:45:32 +0700

    feat: add notification page

---
+++ src/components/notification/NotificationList.tsx
@@ -0,0 +1,12 @@
+export default function NotificationList() {
+  return (
+    <main className="w-[90svw]flex flex-col items-center justify-start grow">
+      <div className="mx-auto px-8">
+        <h1 className="text-3xl font-bold mb-4">Notification List</h1>
+        <p>
+          This is the notification list page. Content will be added here soon.
+        </p>
+      </div>
+    </main>
+  );
+}
