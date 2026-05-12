ab7f4fdd947b377ce494f85061c57e1c04d3388d
Sandi Miftah <sandiseeyou@gmail.com>
2026-05-12 23:42:23 +0700

    feat: add landing page

---
+++ src/app/(campaign)/page.tsx
@@ -0,0 +1,10 @@
+export default function CampaignPage() {
+  return (
+    <main className="w-[90svw]flex flex-col items-center justify-start grow">
+      <div className="mx-auto px-8">
+        <h1 className="text-3xl font-bold mb-4">Campaign Page</h1>
+        <p>This is the campaign page. Content will be added here soon.</p>
+      </div>
+    </main>
+  );
+}
